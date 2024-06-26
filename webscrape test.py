75e3833281e21b7af848eb891092500b8d8f7a6b
owen-lai <124204269+owen-lai@users.noreply.github.com>
2024-06-26 08:55:30 +0000

    scraped the summary of movie

--- webscrape test.py
+++ webscrape test.py
@@ -4,5 +4,14 @@ url = 'https://www.imdb.com/title/tt22022452/plotsummary/'
 headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/126.0.0.0 Safari/537.36 Edg/126.0.0.0'}
 response = requests.get(url, headers=headers)
  
-print('Status code: ', response.status_code)
-print('Text: ', response.text[:1000])
+# print('Status code: ', response.status_code)
+# print('Text: ', response.text[:1000])
+
+from bs4 import BeautifulSoup as bs
+ 
+# Parse the HTML
+soup = bs(response.text, 'html.parser')
+ 
+# Extract any HTML tag
+title = soup.find('div', {'class': 'ipc-html-content-inner-div'})
+print(title)
