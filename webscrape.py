3e6ff9c53175a9a0a1593d7cb8f18c6040e31fcf
yutahata <23hatay@asij.ac.jp>
2024-07-01 23:12:41 -0400

    test

--- webscrape.py
+++ webscrape.py
@@ -3,6 +3,7 @@ import numpy as np
 import requests
 from bs4 import BeautifulSoup as bs
 from bs4 import SoupStrainer as ss
+from concurrent.futures import ThreadPoolExecutor
 
 df = pd.read_csv('movies.csv')
 
@@ -16,18 +17,32 @@ urls = ["https://www.imdb.com/title/" + s + "/plotsummary/" for s in moviesIds]
 # url = 'https://www.imdb.com/title/tt0072308/plotsummary/'
 headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/126.0.0.0 Safari/537.36 Edg/126.0.0.0'}
 only_sum_tags = ss('div', {'class': 'ipc-html-content-inner-div'})
-summaries = []
-for i in urls[:5]:
-    response = requests.get(i, headers=headers)
+# for i in urls[:5]:
+#     response = requests.get(i, headers=headers)
  
-# print('Status code: ', response.status_code)
-# print('Text: ', response.text[:1000])
+# # print('Status code: ', response.status_code)
+# # print('Text: ', response.text[:1000])
  
-# Parse the HTML
-    soup = bs(response.text, 'lxml', parse_only=only_sum_tags)
+# # Parse the HTML
+#     soup = bs(response.text, 'lxml', parse_only=only_sum_tags)
     
-# Extract any HTML tag
+# # Extract any HTML tag
+#     title = soup.find('div', {'class': 'ipc-html-content-inner-div'})
+#     summaries.append(title)
+#     #print(title)
+
+def scrape(url):
+    response = requests.get(url, headers=headers)
+    soup = bs(response.text, 'lxml', parse_only=only_sum_tags)
     title = soup.find('div', {'class': 'ipc-html-content-inner-div'})
-    summaries.append(title)
-    #print(title)
-print(summaries)
+    return {url: title}
+
+with ThreadPoolExecutor(max_workers=10) as executor:
+    results = list(executor.map(scrape, urls))
+
+combined_results = {}
+for result in results:
+    combined_results.update(result)
+
+for url, data in combined_results.items()[:5]:
+    print(f'{url}: {data}')
